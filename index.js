5431d33bee29ad92e7e524ac433ed61e4719d6c5
creativerze <creativerze@gmail.com>
2022-11-09 15:19:29 +0600

    added delete api

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete
+    app.delete('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send(result);
+    });
+
   }
   finally {
 
