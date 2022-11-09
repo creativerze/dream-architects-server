4713583d41b46dc5acfe4b9ae4820d34fedf05ea
creativerze <creativerze@gmail.com>
2022-11-09 23:30:48 +0600

    compleated update review api

--- index.js
+++ index.js
@@ -78,6 +78,27 @@ async function run() {
       res.send(result);
     });
 
+    // update
+    app.get('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const review = await reviewCollection.findOne(query);
+      res.send(review);
+    });
+
+    app.put('/review/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id) };
+      const previousReview = req.body;
+      const option = { upsert: true };
+      const updatedReview = {
+        $set: {
+          review: previousReview.review
+        }
+      };
+      const result = await reviewCollection.updateOne(filter, updatedReview, option);
+      res.send(result);
+    });
 
 
   }
